b70ea83fc166c79744a39ae165dd1833d62c3b13
Robson Aires <robsonaires@outlook.com.br>
2023-11-25 22:43:37 -0300

    home section finalizada

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './App.css'
 import Header from './components/common/heading/Header'
-import { BrowserRouter as Router, Switch, Route, LInk } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Home from './components/home/home'
 
 const App = () => {
   return (
     <>
-  <Header />
   <Router>
+  <Header />
     <Switch>
-      
+      <Route path='/' exact component={Home} />
     </Switch>
   </Router>
     </>
---
+++ src/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './hero/hero'
+
+const home = () => {
+  return (
+    <>
+        <Hero />
+    </>
+  )
+}
+
+export default home
